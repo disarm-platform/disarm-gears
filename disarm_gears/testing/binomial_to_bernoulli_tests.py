2e2737ae8a43bbc20e91b473d0bf7af0a6d248f9
ric70x7 <ric70x7@gmail.com>
2018-08-10 14:52:13 -0700

    new method for array validation

--- disarm_gears/testing/binomial_to_bernoulli_tests.py
+++ disarm_gears/testing/binomial_to_bernoulli_tests.py
@@ -4,30 +4,38 @@ from disarm_gears.util import binomial_to_bernoulli
 
 
 # Inputs
-bad_n_trials = np.random.uniform(5, 200, 20)
-bad_y = np.hstack([np.random.uniform(1, bi, 1) for bi in bad_n_trials])
-good_n_trials = np.ceil(bad_n_trials)
+bad_n = np.random.uniform(5, 200, 20)
+bad_y = np.hstack([np.random.uniform(1, bi, 1) for bi in bad_n])
+good_n = np.ceil(bad_n)
 good_y = np.floor(bad_y)
 good_X = np.random.normal(0, 1, good_y.size * 2).reshape(-1, 2)
-
+npos_3 = np.array([2, 1, 0])
+ntri_3 = np.array([2, 3, 1])
 
 class BinomialToBernoulliTests(unittest.TestCase):
 
     def test_inputs(self):
 
-        self.assertRaises(AssertionError, binomial_to_bernoulli, y=bad_y, n_trials=good_n_trials)
-        self.assertRaises(AssertionError, binomial_to_bernoulli, y=good_y, n_trials=bad_n_trials)
-        self.assertRaises(AssertionError, binomial_to_bernoulli, y=good_y, n_trials=good_n_trials, X=good_X.flatten())
+        self.assertRaises(AssertionError, binomial_to_bernoulli, n_positive=bad_y, n_trials=good_n)
+        self.assertRaises(AssertionError, binomial_to_bernoulli, n_positive=good_y, n_trials=bad_n)
+        self.assertRaises(AssertionError, binomial_to_bernoulli, n_positive=good_y, n_trials=good_n,
+                          X=good_X.flatten())
 
     def test_outputs(self):
 
-        output_1 = binomial_to_bernoulli(y=good_y, n_trials=good_n_trials, X=good_X)
-        self.assertEqual(len(output_1), 3)
-        self.assertIsInstance(output_1[0], np.ndarray)
-        self.assertIsInstance(output_1[1], np.ndarray)
-        self.assertIsInstance(output_1[2], np.ndarray)
-        self.assertEqual(sum(output_1[1]), sum(good_n_trials))
+        y1, w1, X1 = binomial_to_bernoulli(n_positive=good_y, n_trials=good_n, X=good_X, aggregated=False)
+        self.assertIsInstance(y1, np.ndarray)
+        self.assertIsInstance(w1, np.ndarray)
+        self.assertIsInstance(X1, np.ndarray)
+        self.assertEqual(sum(w1), sum(good_n))
+        self.assertEqual(sum(y1 * w1), sum(good_y))
 
-        output_1 = binomial_to_bernoulli(y=good_y, n_trials=good_n_trials, X=None)
-        self.assertEqual(len(output_1), 3)
+        output_2 = binomial_to_bernoulli(n_positive=good_y, n_trials=good_n, X=None, aggregated=False)
+        self.assertEqual(len(output_2), 3)
 
+        y3, w3, X3 = binomial_to_bernoulli(n_positive=npos_3, n_trials=ntri_3, X=None, aggregated=False)
+        self.assertEqual(y3.size, sum(ntri_3))
+        self.assertTrue((w3 == 1).all())
+        self.assertTrue(sum(y3), sum(ntri_3))
+        self.assertEqual(sum(y3 * w3), sum(npos_3))
+        self.assertIsNone(X3)
--- disarm_gears/util/binomial_to_bernoulli.py
+++ disarm_gears/util/binomial_to_bernoulli.py
@@ -1,56 +1,60 @@
 import numpy as np
+from ..validators import *
 
 
-def binomial_to_bernoulli(y, n_trials, X=None, aggregated=True):
+def binomial_to_bernoulli(n_positive, n_trials, X=None, aggregated=True):
     '''
-    Takes a set binomial observations and returns a set of bernoulli observations weighted by
-    the number of trials.
+    Takes a set binomial observations and returns a set of weighted bernoulli observations.
 
-    :param y: Number of successes in a Binomial experiment.
+    :param n_positive: Number of successes in a Binomial experiment.
               Numpy array, shape = [n, ]
 
     :param n_trials: Number of trials in a Binomial experiment.
                      Numpy array, shape = [n, ]
 
-    :param X: Covariates or features associated to y (optional).
-              Numpy array, shape = [n, ] or [n, d] (defaults to None).
+    :param X: Features associated to n_positive (optional).
+              Numpy array, shape = [n, ] or [n, d] (default None).
+
+    :param aggregated: Whether to aggregate repeated observations.
+                       Bool (default True).
 
     :return: tuple (ones-zeros array [m, ], weights array [m, ], new_X array [m, ] or [m, d])
     '''
 
+    # Validate arrays
+    validate_1d_array(n_positive, size=None)
+    validate_1d_array(n_trials, size=n_positive.size)
+    validate_integer_array(n_positive)
+    validate_integer_array(n_trials)
+    validate_non_negative_array(n_positive)
+    validate_positive_array(n_trials)
 
-    assert y.ndim == 1, 'y is expected to be one-dimensional.'
-    assert n_trials.ndim == 1, 'n_trials is expected to be one-dimensional.'
-    assert y.size == n_trials.size, 'y and n_trials sizes do not match.'
-    assert (np.round(y) - y).sum(0) == 0, 'y is expected to be an array of integers.'
-    assert (np.round(n_trials) - n_trials).sum(0) == 0, 'n_trials is expected to be an array of integers.'
-    assert sum(y < 0) == 0, 'y is expected to be non-negative.'
-    assert sum(n_trials <= 0) == 0, 'n_trials is expected to be positive.'
+    positive_ix = n_positive > 0
+    negative_ix = n_trials > n_positive
 
-    positive_ix = y > 0
-    negative_ix = n_trials > y
-
-    weights = np.hstack([y[positive_ix], (n_trials-y)[negative_ix]]).astype(float)
-    new_y = np.hstack([np.ones(sum(positive_ix)), np.zeros(sum(negative_ix))])
+    weights = np.hstack([n_positive[positive_ix], (n_trials-n_positive)[negative_ix]]).astype(float)
+    target = np.hstack([np.ones(sum(positive_ix)), np.zeros(sum(negative_ix))])
     new_X = None
 
     if X is not None:
         if X.ndim == 1:
-            assert X.size == y.size, 'y and X sizes do not match.'
+            assert X.size == n_positive.size, 'n_positive and X sizes do not match.'
             new_X = np.hstack([X[positive_ix], X[negative_ix]])
         elif X.ndim == 2:
-            assert X.shape[0] == y.size, 'y size and X dimensions do not match.'
+            assert X.shape[0] == n_positive.size, 'n_positive size and X dimensions do not match.'
             new_X = np.vstack([X[positive_ix], X[negative_ix]])
         else:
             raise ValueError('X dimensions were not understood.')
 
-    if not aggregated: #TODO this is not tested yet
+    if not aggregated:
+        # Repeat 0-1 as many times as n_positive-n_trials and add weight one to each of them.
 
         if X is not None:
             new_X = np.vstack([np.repeat(_x, _w).reshape(new_X.shape[1], -1).T for
                                _x,_w in zip(new_X, weights)])
-        new_y = np.hstack([_y * np.ones(_w) for _y,_w in zip(new_y, weights.astype(int))])
-        weights = np.ones_like(new_y)
 
-    return new_y, weights, new_X
+        target = np.hstack([_y * np.ones(_w) for _y,_w in zip(target, weights.astype(int))])
+        weights = np.ones_like(target)
+
+    return target, weights, new_X
 
