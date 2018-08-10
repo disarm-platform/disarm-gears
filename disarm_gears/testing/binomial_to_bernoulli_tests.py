d634bee8ba27f021676d063b1ec1c47dc2044b36
ric70x7 <ric70x7@gmail.com>
2018-08-09 17:40:48 -0700

    relative import changed

--- disarm_gears/testing/binomial_to_bernoulli_tests.py
+++ disarm_gears/testing/binomial_to_bernoulli_tests.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from ..util import binomial_to_bernoulli
+from disarm_gears.util import binomial_to_bernoulli
 
 
 # Inputs
@@ -15,25 +15,19 @@ class BinomialToBernoulliTests(unittest.TestCase):
 
     def test_inputs(self):
 
-        self.assertRaises(AssertionError, binomial_data_to_bernoulli, y=bad_y,
-                          n_trials=good_n_trials)
-        self.assertRaises(AssertionError, binomial_data_to_bernoulli, y=good_y,
-                          n_trials=bad_n_trials)
-        self.assertRaises(AssertionError, binomial_data_to_bernoulli, y=good_y,
-                          n_trials=good_n_trials,
-                          X=good_X.flatten())
+        self.assertRaises(AssertionError, binomial_to_bernoulli, y=bad_y, n_trials=good_n_trials)
+        self.assertRaises(AssertionError, binomial_to_bernoulli, y=good_y, n_trials=bad_n_trials)
+        self.assertRaises(AssertionError, binomial_to_bernoulli, y=good_y, n_trials=good_n_trials, X=good_X.flatten())
 
     def test_outputs(self):
 
-        output_1 = binomial_data_to_bernoulli(y=good_y, n_trials=good_n_trials,
-                                              X=good_X)
+        output_1 = binomial_to_bernoulli(y=good_y, n_trials=good_n_trials, X=good_X)
         self.assertEqual(len(output_1), 3)
         self.assertIsInstance(output_1[0], np.ndarray)
         self.assertIsInstance(output_1[1], np.ndarray)
         self.assertIsInstance(output_1[2], np.ndarray)
         self.assertEqual(sum(output_1[1]), sum(good_n_trials))
 
-        output_1 = binomial_data_to_bernoulli(y=good_y, n_trials=good_n_trials,
-                                              X=None)
+        output_1 = binomial_to_bernoulli(y=good_y, n_trials=good_n_trials, X=None)
         self.assertEqual(len(output_1), 3)
 
