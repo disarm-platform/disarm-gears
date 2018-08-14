39d40b8342783d1d4179f58379de781a7532e7d2
ric70x7 <ric70x7@gmail.com>
2018-08-14 15:04:32 -0700

    assertions added in validation of train/predict

--- disarm_gears/chain_drives/chain_drive.py
+++ disarm_gears/chain_drives/chain_drive.py
@@ -7,12 +7,13 @@ class ChainDrive(object):
 
     def __init__(self, base_model_gen, x_norm_gen=None):
 
-        # TODO check base_model_gen
+        assert callable(base_model_gen), 'Object is not callable'
         self.new_base_model = base_model_gen
 
         # Define x normalizer
         if x_norm_gen is not None:
-            assert isinstance(x_norm_gen, StandardScaler) or isinstance(x_norm_gen, Normalizer)
+            assert callable(x_norm_gen), 'Object is not callable'
+            assert isinstance(x_norm_gen(), StandardScaler) or isinstance(x_norm_gen(), Normalizer)
             self.x_norm = True
             self.new_x_norm_rule = x_norm_gen
         else:
@@ -71,13 +72,17 @@ class ChainDrive(object):
         # Validate arrays with x_variables
         x_size = None
 
-        if x_coords is not None:
+        if self.spatial:
             validate_2d_array(x_coords, n_rows=x_size, n_cols=2)
             x_size = x_coords.shape[0]
+        else:
+            assert x_coords is None
 
         if self.temporal:
             validate_1d_array(x_time, size=x_size)
             x_size = x_time.size if x_size is None else x_size
+        else:
+            assert x_time is None
 
         if self.n_features == 1:
             validate_1d_array(x_features, size=x_size)
@@ -85,6 +90,8 @@ class ChainDrive(object):
         elif self.n_features > 1:
             validate_2d_array(x_features, n_cols=self.n_features, n_rows=x_size)
             x_size = x_features.shape[0] if x_size is None else x_size
+        else:
+            assert x_features is None
 
         if exposure is not None:
             validate_1d_array(exposure, size=x_size)
@@ -101,24 +108,28 @@ class ChainDrive(object):
         x_list = list()
 
         # Coords columns
-        if self.spatial:
+        if x_coords is not None:
             x_list.append(x_coords)
 
         # Time column
-        if self.temporal:
+        if x_time is not None:
             x_list.append(x_time[:, None])
 
         # Feature columns
-        if self.n_features == 1:
-            x_list.append(x_features[:, None])
-        elif self.n_features > 1:
-            x_list.append(x_features)
+        if x_features is not None:
+            if x_features.ndim == 1:
+                x_list.append(x_features[:, None])
+            else:
+                x_list.append(x_features)
+
+        assert len(set([xi.shape[0] for xi in x_list])) == 1, 'x_variables have different sizes.'
 
         return np.hstack(x_list)
 
 
     def _preprocess_train_x_variables(self, x_coords, x_time, x_features, overwrite=False):
         '''Preprocessing of x_variables for new training.'''
+
         X = self._stack_x(x_coords=x_coords, x_time=x_time, x_features=x_features)
 
         # If normalization is required
@@ -180,6 +191,9 @@ class ChainDrive(object):
         # Train model
         new_base_model = self._fit_base_model(y=y, X=X, weights=w, exposure=e)
 
+        # Add some summary
+        #TODO
+
         if overwrite:
             self.base_model = new_base_model
         else:
@@ -190,6 +204,9 @@ class ChainDrive(object):
         '''
         Make predictions
         '''
+        # Check there is a trained model
+        assert hasattr(self, 'base_model'), 'A base_model has not been trained yet.'
+
         # Validate inputs
         self._validate_prediction_inputs_dims(x_coords=x_coords,
                                               x_time=x_time,
@@ -208,6 +225,9 @@ class ChainDrive(object):
         '''
         Generate samples from the posterior distribution
         '''
+        # Check there is a trained model
+        assert hasattr(self, 'base_model'), 'A base_model has not been trained yet.'
+
         # Validate inputs
         self._validate_prediction_inputs_dims(x_coords=x_coords,
                                               x_time=x_time,
@@ -223,12 +243,12 @@ class ChainDrive(object):
 
 
     def _fit_base_model(self, y, X, weights, exposure):
-        raise NotImplemented
+        raise NotImplementedError
 
 
-    def _predict_base_model(self, y, X, exposure):
-        raise NotImplemented
+    def _predict_base_model(self, X, exposure):
+        raise NotImplementedError
 
 
-    def _posterior_samples_base_model(self, y, X, exposure):
-        raise NotImplemented
+    def _posterior_samples_base_model(self, X, exposure, n_samples):
+        raise NotImplementedError
