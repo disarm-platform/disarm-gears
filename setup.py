27331ad7e24479f2d8daffab5459b97a8eddf201
ric70x7 <ric70x7@gmail.com>
2018-08-16 08:12:34 -0700

    store _train data

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+__version__ = '.0.1.dev0'
+
+setup(name = 'DisarmGears',
+      version = __version__,
+      description = "(Disarm's Brains on Demand Toolbox)",
+      packages = ['DisarmGears',],
+      license = 'BSD 3-clause',
+      long_description=open('README.md').read(),
+      )
