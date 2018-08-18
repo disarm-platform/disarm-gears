e6de30a715c7c31512d4d6d4c1fbbcd9a20db10d
ric70x7 <ric70x7@gmail.com>
2018-08-17 17:51:03 -0700

    update

--- disarm_gears/gears/__init__.py
+++ disarm_gears/gears/__init__.py
@@ -1,3 +1,3 @@
-from disarm_gears.gears.gaussian_process import GaussianProcess
-from disarm_gears.gears.prevalence_model import PrevalenceModel
+from disarm_gears.gears.gpy_regression import GPyRegression
+from disarm_gears.gears.gpy_regression_proportions import GPyRegressionProportions
 from disarm_gears.gears.spatial_samplers import *
