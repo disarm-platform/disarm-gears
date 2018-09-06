584bf0a607dc2744d38179786b67d92af1b04504
ric70x7 <ric70x7@gmail.com>
2018-09-06 10:44:23 -0700

    all gears removed

--- disarm_gears/gears/__init__.py
+++ disarm_gears/gears/__init__.py
@@ -1,3 +1,4 @@
-from disarm_gears.gears.spatial_samplers import *
-from disarm_gears.gears.gpy_plugins import *
-from disarm_gears.gears.stan_plugins import *
+#from disarm_gears.gears.spatial_samplers import *
+#from disarm_gears.gears.pygam_plugins import *
+#from disarm_gears.gears.gpy_plugins import *
+#from disarm_gears.gears.stan_plugins import *
