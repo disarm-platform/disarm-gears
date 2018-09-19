c415b1be3edb4c6aecc7a26026c67bca02caf9c9
ric70x7 <ric70x7@gmail.com>
2018-09-19 13:01:22 -0700

    re-arrangement to allow not using covariates

--- disarm_gears/chain_drives/prototypes/prototype_adaptive.py
+++ disarm_gears/chain_drives/prototypes/prototype_adaptive.py
@@ -11,7 +11,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
 def adaptive_prototype_0(x_frame, x_id, x_coords, n_positive, n_trials,
-                         threshold=.5, covariate_layers = np.array([1, 4, 12, 15])):
+                         threshold=.5, covariate_layers=np.array([1, 4, 12, 15])):
 
     # Validate inputs
     ## x_frame and x_id
@@ -30,10 +30,11 @@ def adaptive_prototype_0(x_frame, x_id, x_coords, n_positive, n_trials,
     validate_positive_array(n_trials)
     validate_integer_array(n_trials)
     validate_1d_array(n_trials, size=train_size)
+
     ## Request parameters
     assert isinstance(threshold, float)
-    validate_1d_array(covariate_layers) #TODO: check covariates are valid
-
+    if covariate_layers is not None:
+        validate_1d_array(covariate_layers) #TODO: check covariates are valid
 
     # Define tessellation
     ts = Tessellation(x_frame)
@@ -41,37 +42,43 @@ def adaptive_prototype_0(x_frame, x_id, x_coords, n_positive, n_trials,
                       'lat': zi.boundary.coords.xy[1].tolist()}
                  for zi, id in zip(ts.region.geometry, x_id)}
 
-
-    # Find covariates
-    headers = {'Content-Type': 'application/json',
-               'Authorization': 'Simple simlsA153Qc57VmTMdrtHo1nl1n1'}
-    payload_frame = {'lng': x_frame[:, 0].tolist(), 'lat': x_frame[:, 1].tolist(),
-                     'layer_name': covariate_layers.tolist()}
-    payload_train = {'lng': x_coords[:, 0].tolist(), 'lat': x_coords[:, 1].tolist(),
-                     'layer_name': covariate_layers.tolist()}
-
-    algo_link = 'https://api.algorithmia.com/v1/algo/hughsturrock/covariate_extractor/0.1.2'
-    algo_frame = requests.post(algo_link, data=json.dumps(payload_frame), headers=headers)
-    algo_train = requests.post(algo_link, data=json.dumps(payload_train), headers=headers)
-
-    cov_frame = np.array(pd.DataFrame(algo_frame.json()['result'])).reshape(frame_size, -1)
-    cov_train = np.array(pd.DataFrame(algo_train.json()['result'])).reshape(train_size, -1)
-
     # Preprocess data
     target, weights, X = binomial_to_bernoulli(n_positive=n_positive, n_trials=n_trials,
                                                X=x_coords, aggregated=True)
 
-    # Fit ML models
-    _t, _w, ml_train_X = binomial_to_bernoulli(n_positive=n_positive, n_trials=n_trials,
-                                               X=cov_train, aggregated=True)
+    # Find covariates
+    if covariate_layers is None:
+        new_X = trend_2nd_order(X)
+        new_x_coords = trend_2nd_order(x_frame)
+
+    else:
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': 'Simple simlsA153Qc57VmTMdrtHo1nl1n1'}
+        payload_frame = {'lng': x_frame[:, 0].tolist(), 'lat': x_frame[:, 1].tolist(),
+                         'layer_name': covariate_layers.tolist()}
+        payload_train = {'lng': x_coords[:, 0].tolist(), 'lat': x_coords[:, 1].tolist(),
+                         'layer_name': covariate_layers.tolist()}
+
+        algo_link = 'https://api.algorithmia.com/v1/algo/hughsturrock/covariate_extractor/0.1.2'
+        algo_frame = requests.post(algo_link, data=json.dumps(payload_frame), headers=headers)
+        algo_train = requests.post(algo_link, data=json.dumps(payload_train), headers=headers)
+
+        cov_frame = np.array(pd.DataFrame(algo_frame.json()['result'])).reshape(frame_size, -1)
+        cov_train = np.array(pd.DataFrame(algo_train.json()['result'])).reshape(train_size, -1)
 
 
-    rf = RandomForestClassifier(max_depth=10, random_state=0)
-    rf.fit(X=ml_train_X, y=target)
-    ml_train = rf.predict_proba(ml_train_X)[:, 1:]
-    ml_frame = rf.predict_proba(cov_frame)[:, 1:]
+        # Fit ML models
+        _t, _w, ml_train_X = binomial_to_bernoulli(n_positive=n_positive, n_trials=n_trials,
+                                                   X=cov_train, aggregated=True)
 
-    new_X = np.hstack([trend_2nd_order(X), ml_train])
+
+        rf = RandomForestClassifier(max_depth=10, random_state=0)
+        rf.fit(X=ml_train_X, y=target)
+        ml_train = rf.predict_proba(ml_train_X)[:, 1:]
+        ml_frame = rf.predict_proba(cov_frame)[:, 1:]
+
+        new_X = np.hstack([trend_2nd_order(X), ml_train])
+        new_x_coords = np.hstack([trend_2nd_order(x_frame), ml_frame])
 
 
     # Train model
@@ -79,10 +86,8 @@ def adaptive_prototype_0(x_frame, x_id, x_coords, n_positive, n_trials,
     base_model.gridsearch(y=target, X=new_X, weights=weights)
 
     n_samples = 300
-    new_x_coords = np.hstack([trend_2nd_order(x_frame), ml_frame])
     m_simulations = base_model.sample(X=new_X, y=target, weights=weights, sample_at_X=new_x_coords,
                                       quantity='mu', n_draws=n_samples)
-
     m_prev = m_simulations.mean(0)
     m_prob = (m_simulations > threshold).sum(0) / n_samples
     m_category = np.zeros_like(m_prob)
