352b1d7d90c9b2c4e4668770cc6558c9113bade2
ric70x7 <ric70x7@gmail.com>
2018-09-26 08:49:20 -0700

    new file

---
+++ disarm_gears/testing/raster_image_tests.py
@@ -0,0 +1,18 @@
+'''
+import unittest
+import numpy as np
+import pandas as pd
+from osgeo import gdal, gdal_array
+from disarm_gears.frames import RasterImage
+#import geopandas as geop
+#rom disarm_gears.frames import Tessellation
+
+#TODO we need a dummy raster here
+#dummy_image = np.arange(36).reshape(6, 6)
+#dummy_coords =
+
+class RasterImageTests(unittest.TestCase):
+
+    def test_inputs(self):
+        pass
+'''
