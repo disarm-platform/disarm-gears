203f63bb23ed07536a201a39cdd732ab4639158f
ric70x7 <ric70x7@gmail.com>
2018-09-27 16:03:56 -0700

    Create license

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ __version__ = '0.1'
 
 setup(name = 'DisarmGears',
       version = __version__,
-      description = "(Disarm's Brains on Demand Toolbox)",
+      description = "Disarm's Brains on Demand Toolbox",
+      author = 'DiSARM authors',
+      url = 'www.disarm.io',
       packages = ['DisarmGears',],
-      license = 'BSD 3-clause',
-      long_description=open('README.md').read(),
+      license = 'MIT License',
+      long_description = open('README.md').read(),
       )
