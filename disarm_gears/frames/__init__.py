522e2e721232203face3cbdfaf57ab77a88e9056
ric70x7 <ric70x7@gmail.com>
2018-09-27 17:19:46 -0700

    Include modules

--- disarm_gears/frames/__init__.py
+++ disarm_gears/frames/__init__.py
@@ -1,4 +1,3 @@
 from disarm_gears.frames.timeframe import Timeframe
 from disarm_gears.frames.tessellation import Tessellation
 from disarm_gears.frames.geometry import Geometry
-#from disarm_gears.frames.raster_image import RasterImage
--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from distutils.core import setup
 
-__version__ = '0.1'
+__version__ = "0.1"
 
-setup(name = 'DisarmGears',
+setup(name = "DisarmGears",
       version = __version__,
       description = "Disarm's Brains on Demand Toolbox",
-      author = 'DiSARM authors',
-      author_email = 'ric70x7@gmail.com',
-      url = 'www.disarm.io',
-      packages = ['disarm_gears',],
-      license = open('LICENSE').read(),
-      long_description = open('README').read(),
+      author = "DiSARM authors",
+      author_email = "ric70x7@gmail.com",
+      url = "www.disarm.io",
+      packages = ["disarm_gears",
+                  "disarm_gears.chain_drives",
+                  "disarm_gears.frames",
+                  "disarm_gears.util",
+                  "disarm_gears.testing",
+                  "disarm_gears.validators"],
+      license = open("LICENSE").read(),
+      long_description = open("README").read(),
       )
