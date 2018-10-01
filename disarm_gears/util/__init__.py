c3df47388091141288906e9ce0cd3005dcd93b3d
ric70x7 <ric70x7@gmail.com>
2018-10-01 11:56:34 -0700

    bubbles method added

--- disarm_gears/util/__init__.py
+++ disarm_gears/util/__init__.py
@@ -1,2 +1,3 @@
 from disarm_gears.util.binomial_to_bernoulli import binomial_to_bernoulli
 from disarm_gears.util.surface_trends import trend_1st_order, trend_2nd_order, trend_3rd_order
+from disarm_gears.util.bubbles import bubbles
