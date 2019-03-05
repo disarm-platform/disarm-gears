c9ec47d095d371851d253f9c8a2dc43155118747
ric70x7 <ric70x7@gmail.com>
2019-03-04 17:15:36 -0800

    init files edited to allow only ntd api

---
+++ disarm_gears/chain_drives/prototypes/__init__.py
@@ -0,0 +1,3 @@
+from disarm_gears.chain_drives.prototypes.prototype_adaptive import adaptive_prototype_0
+#from disarm_gears.chain_drives.prototypes.vbd_sentinel import sentinel
+#from disarm_gears.chain_drives.prototypes.sentinel_validation import *
