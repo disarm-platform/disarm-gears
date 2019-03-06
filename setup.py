40e0eb174078b40fd76c212a78e800c5ab823ae9
ric70x7 <ric70x7@gmail.com>
2019-03-06 08:57:33 -0800

    files for packaging code

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+__version__ = "0.1.2"
+
+setup(name = "DisarmGears",
+      version = __version__,
+      description = "Disarm's Brains on Demand Toolbox",
+      author = "DiSARM authors",
+      author_email = "ric70x7@gmail.com",
+      url = "www.disarm.io",
+      packages = ["disarm_gears",
+                  "disarm_gears.chain_drives",
+                  "disarm_gears.chain_drives.prototypes",
+                  "disarm_gears.frames",
+                  "disarm_gears.util",
+                  "disarm_gears.testing",
+                  "disarm_gears.validators"],
+      license = open("LICENSE").read(),
+      long_description = open("README").read(),
+      )
