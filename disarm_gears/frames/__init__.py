a4395a45050c6f567f8c8bb39db220a4cad6eaa9
ric70x7 <ric70x7@gmail.com>
2019-03-25 13:56:49 -0700

    New frame to handle point pattern data

--- disarm_gears/frames/__init__.py
+++ disarm_gears/frames/__init__.py
@@ -1,5 +1,5 @@
 from disarm_gears.frames.timeframe import Timeframe
-#from disarm_gears.frames.point_pattern import PointPattern
+from disarm_gears.frames.point_pattern import PointPattern
 #from disarm_gears.frames.geometry import Geometry
 #from disarm_gears.frames.raster_image import RasterImage
 
