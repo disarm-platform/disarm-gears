717f8fdab5165e3cd42e701571b216fb4f6ff026
ric70x7 <ric70x7@gmail.com>
2019-05-23 14:40:23 -0700

    Tessellation frame removed

--- disarm_gears/frames/__init__.py
+++ disarm_gears/frames/__init__.py
@@ -2,5 +2,3 @@ from disarm_gears.frames.timeframe import Timeframe
 #from disarm_gears.frames.raster_image import RasterLayer
 from disarm_gears.frames.point_pattern import PointPattern
 from disarm_gears.frames.tile_pattern import TilePattern
-
-from disarm_gears.frames.tessellation import Tessellation #TODO this class will be replaced by TilePattern
