34c4f74daa4a5335114475cb0dc072f57ad1be96
ric70x7 <ric70x7@gmail.com>
2019-06-24 14:36:14 -0700

    r_methods stable and tested

--- disarm_gears/r_plugins/r_methods.py
+++ disarm_gears/r_plugins/r_methods.py
@@ -18,51 +18,32 @@ def pdframe2rdframe(data):
 
     :param data: Data to convert into an R dataframe
                  pandas DataFrame
-    :return: R object
+    :return: robjects.vectors.DataFrame
     '''
     # This step is just to make sure pandas2ri.DataFrame works as expected
+    assert isinstance(data, pd.DataFrame)
     data2 = pd.DataFrame({ci: [vi for vi in data[ci]] for ci in data.columns})
     return pandas2ri.DataFrame(data2)
 
 
-def mgcv_get_rho_power_exp2(iter_formula, data, smooth_dim, family='gaussian', weights=None, method='REML'):
-
-    # Fit initial model and check model components are in place
-    m0 = mgcv_fit(iter_formula %'', data=data, family=family, weights=weights, method=method)
-    ix_gcv = get_names(m0).index('gcv.ubre')
-    ix_smooth = get_names(m0).index('smooth')
-    if smooth_dim == 1:
-        ix_gpdefn = get_names(m0[ix_smooth][0]).index('gp.defn')
-        rho0 = m0[ix_smooth][0][ix_gpdefn][1]
-    elif smooth_dim == 2:
-        ix_gpdefn = get_names(m0[ix_smooth][0][0][0]).index('gp.defn')
-        rho0 = m0[ix_smooth][0][0][0][ix_gpdefn][1]
-    else:
-        raise NotImplementedError
-
-    # Grid of values to try
-    if rho0 >= 8:
-        rho_grid = [rho0/(2 ** i) for i in range(int(np.log2(rho0)))[::-1]]
-    else:
-        rho_grid = [i * rho0 / 10. for i in range(1, 11, 2)]
-
-    # Lambda function to fit all models
-    iter_fit = lambda rho_i:  mgcv_fit(iter_formula %', %s, 2' %rho_i,
-                                       data=data, family=family, weights=weights, method=method)
-    gcv_vals = np.hstack([iter_fit(rho_i)[ix_gcv] for rho_i in rho_grid])
-
-    return rho_grid[np.argmin(gcv_vals)]
-
-
 def mgcv_fit(formula, data, family='gaussian', weights=None, method='REML'):
     '''
-    :param formula:
-                    String.
-    :param data:
+    Fit a Generalized Additive Model
+
+    This function is a wrapper of the MGCV package in R.
+    See its definition for more details https://www.rdocumentation.org/packages/mgcv/versions/1.8-28/topics/gam
+
+    :param formula: R formula
+                    string
+    :param data: Data to fit the model to
                  pandas DataFrame
-    :param family:
-    :param method:
-    :return:
+    :param family: Noise model distribution
+                   string. One of 'gaussian', 'binomial', 'poisson' (default 'gaussian').
+    :param weights: Observation weights
+                    numpy ndarray
+    :param method: Smoothing parameter estimation method (see R MGCV documentation)
+                   string, one of 'GCV.Cp', 'GACV.Cp', 'REML', 'P-REML', 'ML' (default 'REML').
+    :return: robjects.vectors.ListVector
     '''
     assert isinstance(data, pd.DataFrame)
 
@@ -82,11 +63,25 @@ def mgcv_fit(formula, data, family='gaussian', weights=None, method='REML'):
     if weights is None:
         gam = rmgcv.gam(formula=rformula, data=rdata, family=rfamily, method=method)
     else:
+        #TODO assert weights
         gam = rmgcv.gam(formula=rformula, data=rdata, family=family, weights=weights, method=method)
     return gam
 
 
 def mgcv_predict(gam, data, response_type='response'):
+    '''
+    Make predictions using a fitted GAM model
+
+    See R MGCV package
+
+    :param gam: A gam model previously fitted
+                robjects.vectors.ListVector
+    :param data: Input data points where predictions are made
+                 pandas DataFrame
+    :param response_type: Space or transformation in which the prediction is returned (see R MGCV documentation)
+                          string. One of 'response', 'link', 'lpmatrix'.
+    :return: numpy ndarray of shape (n_data, ) with the posterior mean values.
+    '''
 
     assert isinstance(gam, robjects.vectors.ListVector)
     assert isinstance(data, pd.DataFrame)
@@ -97,6 +92,21 @@ def mgcv_predict(gam, data, response_type='response'):
 
 
 def mgcv_posterior_samples(gam, data, n_samples=100, response_type='inverse_link') -> np.ndarray:
+    '''
+    Generate samples from the posterior distribution of a GAM
+
+    See R MGCV package
+
+    :param gam: A gam model previously fitted
+                robjects.vectors.ListVector
+    :param data: Input data points where simulations are generated
+                 Pandas DataFrame
+    :param n_samples: Number of samples to generate
+                      Integer (default 100)
+    :param response_type: Space or transformation in which the prediction is returned (see R MGCV documentation)
+                          String. One of 'response', 'link', 'lpmatrix'.
+    :return: numpy ndarray of shape (n_samples, n_data)
+    '''
 
     assert isinstance(gam, robjects.vectors.ListVector)
     assert isinstance(data, pd.DataFrame)
@@ -115,19 +125,20 @@ def mgcv_posterior_samples(gam, data, n_samples=100, response_type='inverse_link
 
     elif response_type == 'inverse_link':
         family, link = get_family(gam), get_link(gam)
-        if family == 'gaussian' and link == 'identity':
+        if family == 'gaussian':
+            assert link == 'identity'
             samples = _post
-        elif family == 'binomial' and link == 'logit':
+        elif family == 'binomial':
+            assert link == 'logit'
             samples = 1. / (1. + np.exp(-_post))
-        elif family == 'poisson' and link == 'log':
+        else: #family == 'poisson'
+            assert link == 'log'
             samples = np.exp(-_post)
-        else:
-            raise NotImplementedError
 
-    elif response_type == 'response':
+    elif response_type == 'response': #TODO the results accuracy of this case have not been tested yet
         family, link = get_family(gam), get_link(gam)
         if family == 'gaussian' and link == 'identity':
-            ix_scale = get_names(gam).index('scale') #TODO check
+            ix_scale = get_names(gam).index('scale') #TODO check this line
             sigma_noise = np.sqrt(gam[ix_scale])
             samples = _post + np.random.normal(0, sigma_noise, _post.size).reshape(_post.shape)
         else:
@@ -140,22 +151,89 @@ def mgcv_posterior_samples(gam, data, n_samples=100, response_type='inverse_link
 
 
 def get_family(gam):
+    '''
+    Get family of a GAM
+
+    :param gam: A gam model previously fitted
+                robjects.vectors.ListVector
+
+    :return: string
+    '''
     return gam[3][0][0]
 
+
 def get_link(gam):
+    '''
+    Get link function of a GAM
+
+    :param gam: A gam model previously fitted
+                robjects.vectors.ListVector
+
+    :return: string
+    '''
     return gam[3][1][0]
 
+
 def get_names(obj):
-    #return rbase.names(obj)
+    '''
+    Get name of an R object
+
+    :param obj: Object to extract the names from
+                R object
+    :return: List of names
+    '''
     obj_names = rbase.names(obj)
-    if isinstance(obj_names, np.ndarray):
-        obj_names = obj_names.tolist()
-    return obj_names
-
-
-
+    return list(obj_names)
 
 
 def summary(model):
+    '''Print R model summary'''
     print(rbase.summary(model))
+
+
+def mgcv_get_rho_power_exp2(iter_formula, data, smooth_dim, family='gaussian', weights=None, method='REML'):
+    '''
+    For a GAM model with gp smooth of type exponentiated quadratic find the best lengthscale (rho) parameter
+
+    :param iter_formula: R formula
+                         String
+    :param data: Data to fit the model to
+                 pandas DataFrame
+    :param smooth_dim: Dimensionality of the smooth interpolation. The only values accepted are 1 and 2.
+                 Integer
+    :param family: Noise model distribution (see mgcv_fit)
+                   String
+    :param weights: Observation weights (see mgcv_fit)
+                    Numpy ndarray
+    :param method: Smoothing parameter estimation method (see mgcv_fit).
+                   String, defaults to 'REML'.
+    :return: float
+    '''
+
+    # Fit initial model and check model components are in place
+    m0 = mgcv_fit(iter_formula %'', data=data, family=family, weights=weights, method=method)
+    ix_gcv = get_names(m0).index('gcv.ubre')
+    ix_smooth = get_names(m0).index('smooth')
+    if smooth_dim == 1:
+        raise NotImplementedError
+        #ix_gpdefn = get_names(m0[ix_smooth][0]).index('gp.defn')
+        #rho0 = m0[ix_smooth][0][ix_gpdefn][1]
+    elif smooth_dim == 2:
+        ix_gpdefn = get_names(m0[ix_smooth][0][0][0]).index('gp.defn')
+        rho0 = m0[ix_smooth][0][0][0][ix_gpdefn][1]
+    else:
+        raise NotImplementedError
+
+    # Grid of values to try
+    if rho0 >= 8:
+        rho_grid = [rho0/(2 ** i) for i in range(int(np.log2(rho0)))[::-1]]
+    else:
+        rho_grid = [i * rho0 / 10. for i in range(1, 11, 2)]
+
+    # Lambda function to fit all models
+    iter_fit = lambda rho_i:  mgcv_fit(iter_formula %', %s, 2' %rho_i,
+                                       data=data, family=family, weights=weights, method=method)
+    gcv_vals = np.hstack([iter_fit(rho_i)[ix_gcv] for rho_i in rho_grid])
+
+    return rho_grid[np.argmin(gcv_vals)]
 
---
+++ disarm_gears/testing/test_r_methods.py
@@ -0,0 +1,188 @@
+import pytest
+import numpy as np
+import pandas as pd
+import rpy2.robjects as robjects
+from sklearn.datasets import make_blobs
+from sklearn.neighbors import KernelDensity
+from disarm_gears.r_plugins import r_methods
+from rpy2.robjects.packages import importr
+rstats = importr('stats')
+rbase = importr('base')
+
+np.random.seed(10)
+
+## Blobs and grid
+X = make_blobs(n_samples=120, n_features=2, centers=10, cluster_std=.6)[0]
+f_size = 15
+g_size = 50
+f1, f2 = np.meshgrid(np.linspace(-9.6, 9.6, f_size), np.linspace(-9.6, 9.6, f_size))
+g1, g2 = np.meshgrid(np.linspace(-10, 10, g_size), np.linspace(-10, 10, g_size))
+
+
+## Density
+bw = 2
+kde = KernelDensity(bandwidth=bw, kernel='gaussian', algorithm='ball_tree')
+kde.fit(X)
+hf = np.exp(kde.score_samples(np.vstack([f1.flatten(), f2.flatten()]).T))
+hg = np.exp(kde.score_samples(np.vstack([g1.flatten(), g2.flatten()]).T))
+a = hg.max()
+hg = hg / a
+hf = hf / a
+
+x1 = np.random.normal(0, .1, hf.size)
+x2 = np.random.normal(0, .1, hf.size)
+y = .5 * x1 + .2 * x2 + hf.ravel()
+
+
+f_data = pd.DataFrame({'lng': f1.ravel(), 'lat': f2.ravel(), 'x1': x1, 'x2': x2, 'y': y})
+f_data2 = f_data.copy()
+f_data2.loc[:, 'y'] = np.round(100 * 1. / (1. + np.exp(-f_data2.y)))
+f_data2['n'] = 100
+
+f_data3 = f_data.copy()
+f_data3.loc[:, 'lng'] = f_data.lng / 20
+f_data3.loc[:, 'lat'] = f_data.lat / 20
+
+
+def test_pdframe2rdframe():
+
+    with pytest.raises(AssertionError):
+        r_methods.pdframe2rdframe(np.array(f_data))
+
+    rdframe = r_methods.pdframe2rdframe(f_data)
+    assert rdframe.ncol == 5
+    assert np.all([ci in ['lng', 'lat', 'x1', 'x2', 'y'] for ci in rdframe.colnames])
+    assert rdframe.nrow == 225
+    assert isinstance(rdframe, robjects.vectors.DataFrame)
+
+
+def test_mgcv_fit():
+
+
+    formula_gauss = "y ~ x1 + x2 + te(lng, lat, bs='gp', m=list(c(2, 4, 2)), d=2, k=-1)"
+    formula_binom = "cbind(y, 100 - y) ~ x1 + x2 + te(lng, lat, bs='gp', m=list(c(2, 4, 2)), d=2, k=-1)"
+    formula_poiss = "y ~ offset(log(n)) + x1 + x2 + te(lng, lat, bs='gp', m=list(c(2, 4, 2)), d=2, k=-1)"
+    weights = np.random.uniform(.1, 1, f_data2.shape[0])
+
+    m_binom = r_methods.mgcv_fit(formula_binom, f_data2, family='binomial', weights=None, method='REML')
+    m_poiss = r_methods.mgcv_fit(formula_poiss, f_data2, family='poisson', weights=None, method='REML')
+    m_gauss = r_methods.mgcv_fit(formula_gauss, f_data2, family='gaussian', weights=weights, method='REML')
+
+    assert isinstance(m_binom, robjects.vectors.ListVector)
+    assert isinstance(m_poiss, robjects.vectors.ListVector)
+    assert isinstance(m_gauss, robjects.vectors.ListVector)
+
+    with pytest.raises(NotImplementedError):
+        r_methods.mgcv_fit(formula_binom, f_data2, family='xx', weights=None, method='REML')
+
+    with pytest.raises(NotImplementedError):
+        r_methods.mgcv_fit(formula_binom, f_data2, family='xx', weights=weights[:40], method='REML')
+    with pytest.raises(NotImplementedError):
+        r_methods.mgcv_fit(formula_binom, f_data2, family='xx', weights=weights[:, None], method='REML')
+    with pytest.raises(NotImplementedError):
+        r_methods.mgcv_fit(formula_binom, f_data2, family='xx', weights=list(weights), method='REML')
+
+
+def test_mgcv_predict():
+
+    formula_binom = "cbind(y, 100 - y) ~ x1 + x2 + te(lng, lat, bs='gp', m=list(c(2, 4, 2)), d=2, k=-1)"
+    m_binom = r_methods.mgcv_fit(formula_binom, f_data2, family='binomial', weights=None, method='REML')
+    response = r_methods.mgcv_predict(m_binom, f_data2, response_type='response')
+    link = r_methods.mgcv_predict(m_binom, f_data2, response_type='link')
+    lpmatrix = r_methods.mgcv_predict(m_binom, f_data2, response_type='lpmatrix')
+
+    assert isinstance(response, np.ndarray)
+    assert isinstance(link, np.ndarray)
+    assert isinstance(lpmatrix, np.ndarray)
+    assert response.size == f_data2.shape[0]
+    assert link.size == f_data2.shape[0]
+    assert lpmatrix.shape[0] == f_data2.shape[0]
+    assert lpmatrix.shape[1] == len(rstats.coef(m_binom))
+
+    with pytest.raises(NotImplementedError):
+        r_methods.mgcv_predict(m_binom, f_data2, response_type='xx')
+
+
+def test_mgcv_posterior_samples():
+
+    formula_gauss = "y ~ x1 + x2 + te(lng, lat, bs='gp', m=list(c(2, 4, 2)), d=2, k=-1)"
+    formula_binom = "cbind(y, 100 - y) ~ x1 + x2 + te(lng, lat, bs='gp', m=list(c(2, 4, 2)), d=2, k=-1)"
+    formula_poiss = "y ~ offset(log(n)) + x1 + x2 + te(lng, lat, bs='gp', m=list(c(2, 4, 2)), d=2, k=-1)"
+    m_gauss = r_methods.mgcv_fit(formula_gauss, f_data2, family='gaussian', weights=None, method='REML')
+    m_binom = r_methods.mgcv_fit(formula_binom, f_data2, family='binomial', weights=None, method='REML')
+    m_poiss = r_methods.mgcv_fit(formula_poiss, f_data2, family='poisson', weights=None, method='REML')
+    link = r_methods.mgcv_posterior_samples(m_binom, f_data2, n_samples=100, response_type='link')
+    ilink_gauss = r_methods.mgcv_posterior_samples(m_gauss, f_data2, n_samples=100, response_type='inverse_link')
+    ilink_binom = r_methods.mgcv_posterior_samples(m_binom, f_data2, n_samples=100, response_type='inverse_link')
+    ilink_poiss = r_methods.mgcv_posterior_samples(m_poiss, f_data2, n_samples=100, response_type='inverse_link')
+    resp_gauss = r_methods.mgcv_posterior_samples(m_gauss, f_data2, n_samples=100, response_type='response')
+
+    assert isinstance(link, np.ndarray)
+    assert link.shape[0] == 100
+    assert link.shape[1] == f_data2.shape[0]
+
+    assert isinstance(ilink_gauss, np.ndarray)
+    assert isinstance(ilink_binom, np.ndarray)
+    assert isinstance(ilink_poiss, np.ndarray)
+
+    assert isinstance(resp_gauss, np.ndarray)
+
+    with pytest.raises(NotImplementedError):
+        r_methods.mgcv_posterior_samples(m_binom, f_data2, n_samples=100, response_type='xx')
+
+    with pytest.raises(NotImplementedError):
+        r_methods.mgcv_posterior_samples(m_binom, f_data2, n_samples=100, response_type='response')
+
+
+def test_mgcv_get_rho_power_exp2():
+
+    formula_gauss = "y ~ x1 + x2 + te(lng, lat, bs='gp', m=list(c(2%s)), d=2, k=-1)"
+
+    with pytest.raises(NotImplementedError):
+        r_methods.mgcv_get_rho_power_exp2(iter_formula=formula_gauss, data=f_data, smooth_dim=1, family='gaussian',
+                                          weights=None, method="REML")
+    with pytest.raises(NotImplementedError):
+        r_methods.mgcv_get_rho_power_exp2(iter_formula=formula_gauss, data=f_data, smooth_dim=3, family='gaussian',
+                                          weights=None, method="REML")
+
+    rho_gauss = r_methods.mgcv_get_rho_power_exp2(iter_formula=formula_gauss, data=f_data, smooth_dim=2, family='gaussian',
+                                                  weights=None, method="REML")
+    rho_gauss3 = r_methods.mgcv_get_rho_power_exp2(iter_formula=formula_gauss, data=f_data3, smooth_dim=2, family='gaussian',
+                                                  weights=None, method="REML")
+
+    assert isinstance(rho_gauss, float)
+
+
+def test_get_family():
+
+    formula_binom = "cbind(y, 100 - y) ~ x1 + x2 + te(lng, lat, bs='gp', m=list(c(2, 4, 2)), d=2, k=-1)"
+    m_binom = r_methods.mgcv_fit(formula_binom, f_data2, family='binomial', weights=None, method='REML')
+    family = r_methods.get_family(m_binom)
+    assert family == 'binomial'
+
+def test_get_link():
+
+    formula_binom = "cbind(y, 100 - y) ~ x1 + x2 + te(lng, lat, bs='gp', m=list(c(2, 4, 2)), d=2, k=-1)"
+    m_binom = r_methods.mgcv_fit(formula_binom, f_data2, family='binomial', weights=None, method='REML')
+    link = r_methods.get_link(m_binom)
+    assert link == 'logit'
+
+
+def test_get_names():
+
+    rdframe = r_methods.pdframe2rdframe(f_data)
+    obj_names1 = r_methods.get_names(rdframe)
+    assert isinstance(obj_names1, list)
+
+    formula_binom = "cbind(y, 100 - y) ~ x1 + x2 + te(lng, lat, bs='gp', m=list(c(2, 4, 2)), d=2, k=-1)"
+    m_binom = r_methods.mgcv_fit(formula_binom, f_data2, family='binomial', weights=None, method='REML')
+    obj_names2 = r_methods.get_names(m_binom)
+    assert isinstance(obj_names2, list)
+
+def test_summary(capfd):
+
+    formula_binom = "cbind(y, 100 - y) ~ x1 + x2 + te(lng, lat, bs='gp', m=list(c(2, 4, 2)), d=2, k=-1)"
+    m_binom = r_methods.mgcv_fit(formula_binom, f_data2, family='binomial', weights=None, method='REML')
+    r_methods.summary(m_binom)
+    out, err = capfd.readouterr()
+    assert out is not None
