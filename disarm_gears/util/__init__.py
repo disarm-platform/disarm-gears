160a99ba13e5d260603f62ca23d83c72e77b3817
ric70x7 <ric70x7@gmail.com>
2019-06-24 14:32:00 -0700

    json_helpers not needed now

--- disarm_gears/util/__init__.py
+++ disarm_gears/util/__init__.py
@@ -1,4 +1,4 @@
 from disarm_gears.util.binomial_to_bernoulli import binomial_to_bernoulli
 from disarm_gears.util.buffers import voronoi_polygons, regular_polygons, disjoint_polygons
 from disarm_gears.util.surface_trends import trend_1st_order, trend_2nd_order, trend_3rd_order
-from disarm_gears.util.json_helpers import *
+#from disarm_gears.util.json_helpers import *
--- disarm_gears/util/json_helpers.py
+++ disarm_gears/util/json_helpers.py
@@ -1,83 +1,69 @@
-import json
-import pandas as pd
-
-
-def geojson_encoder_1(dataframe, layer_names=[], lng='lng', lat='lat'):
-    #TODO this will be replaced by encoder_3
-    '''
-    Put dataframe into geojson format
-
-    :param dataframe: Data to put into geojson format.
-                      pandas DataFrame.
-    :param layer_names:
-                        List of strings.
-    :param lng: Name of longitude variable within dataframe.
-                String, defaults to 'lng'.
-    :param lat: Name of latitude variable within dataframe.
-                String, defaults to 'lat'.
-    '''
-    #TODO add tests
-    geojson_dict = {'type': 'FeatureCollection', 'features': []}
-    for _, row in dataframe.iterrows():
-        _feature = {'type': 'Feature', 'properties': {}, 'geometry': {'type': 'Point', 'coordinates': []}}
-        _feature['geometry']['coordinates'] = [row[lng], row[lat]]
-        geojson_dict['features'].append(_feature)
-
-    new_json = {'layer_names': layer_names, 'points': geojson_dict}
-
-    return json.dumps(new_json)
-
-
-def geojson_encoder_2(dataframe, fields=[], lng='lng', lat='lat', dumps=False):
-    '''
-    Put dataframe into geojson format
-
-    :param dataframe: Data to put into geojson format.
-                      pandas DataFrame.
-    :param fields:
-                        List of strings.
-    :param lng: Name of longitude variable within dataframe.
-                String, defaults to 'lng'.
-    :param lat: Name of latitude variable within dataframe.
-                String, defaults to 'lat'.
-    '''
-    #TODO add tests
-    geojson_dict = {'type': 'FeatureCollection', 'features': []}
-    for _, row in dataframe.iterrows():
-        _feature = {'type': 'Feature', 'properties': row[fields].to_dict(), 'geometry': {'type': 'Point', 'coordinates': []}}
-        _feature['geometry']['coordinates'] = [row[lng], row[lat]]
-        geojson_dict['features'].append(_feature)
-
-    result = {'point_data': geojson_dict}
-
-    if dumps:
-        result = json.dumps(result)
-
-    return result
-
-
-def geojson_encoder_3(dataframe, fields=[], layer_names=[], lng='lng', lat='lat', dumps=False):
-    '''
-    Put dataframe into geojson format
-
-    :param dataframe: Data to put into geojson format.
-                      pandas DataFrame.
-    :param layer_names:
-                        List of strings.
-    :param lng: Name of longitude variable within dataframe.
-                String, defaults to 'lng'.
-    :param lat: Name of latitude variable within dataframe.
-                String, defaults to 'lat'.
-    '''
-    #TODO add tests
-    geojson_dict = {'type': 'FeatureCollection', 'features': []}
-    for _, row in dataframe.iterrows():
-        _feature = {'type': 'Feature', 'properties': row[fields].to_dict(), 'geometry': {'type': 'Point', 'coordinates': []}}
-        _feature['geometry']['coordinates'] = [row[lng], row[lat]]
-        geojson_dict['features'].append(_feature)
-
-    result = {'layer_names': layer_names, 'points': geojson_dict}
-
+#import json
+#import pandas as pd
+#
+##TODO unify terms: 'points', 'point_data', 'region_definition', etc.
+#
+#def geojson_encoder_2(dataframe, fields=[], lng='lng', lat='lat', dumps=False):
+#    '''
+#    Serialize a dataframe as GEOJSON format
+#
+#    :param dataframe: Data source to serialize.
+#                      Pandas DataFrame.
+#    :param fields: Fields from dataframe to encode.
+#                   List of strings.
+#    :param lng: Name of longitude variable within dataframe.
+#                String, defaults to 'lng'.
+#    :param lat: Name of latitude variable within dataframe.
+#                String, defaults to 'lat'.
+#    :param dumps: Whether to return a GEOJSON formatted string (True) or a GEOJSON formatted stream.
+#                  Boolean (default False).
+#
+#    :return: Dictionary (if dumps = False) or string if (dumps = True)
+#    '''
+#    #TODO add tests
+#    geojson_dict = {'type': 'FeatureCollection', 'features': []}
+#    for _, row in dataframe.iterrows():
+#        _feature = {'type': 'Feature', 'properties': row[fields].to_dict(), 'geometry': {'type': 'Point', 'coordinates': []}}
+#        _feature['geometry']['coordinates'] = [row[lng], row[lat]]
+#        geojson_dict['features'].append(_feature)
+#
+#    result = {'point_data': geojson_dict}
+#
+#    if dumps:
+#        result = json.dumps(result)
+#
+#    return result
+#
+#
+#def geojson_encoder_3(dataframe, fields=[], layer_names=[], lng='lng', lat='lat', dumps=False):
+#    '''
+#    Serialize a dataframe as GEOJSON format
+#
+#    :param dataframe: Data source to serialize.
+#                      Pandas DataFrame.
+#    :param fields: Fields from dataframe to encode.
+#                   List of strings.
+#    :param layer_names: Additional fields to include into the GEOJSON.
+#                        List of strings.
+#    :param lng: Name of longitude variable within dataframe.
+#                String, defaults to 'lng'.
+#    :param lat: Name of latitude variable within dataframe.
+#                String, defaults to 'lat'.
+#    :param dumps: Whether to return a JSON formatted string (True) or a JSON formatted stream.
+#                  Boolean (default False).
+#
+#    :return: Dictionary (if dumps = False) or string if (dumps = True)
+#    '''
+#
+#    #TODO add tests
+#    geojson_dict = {'type': 'FeatureCollection', 'features': []}
+#    for _, row in dataframe.iterrows():
+#        _feature = {'type': 'Feature', 'properties': row[fields].to_dict(), 'geometry': {'type': 'Point', 'coordinates': []}}
+#        _feature['geometry']['coordinates'] = [row[lng], row[lat]]
+#        geojson_dict['features'].append(_feature)
+#
+#    result = {'layer_names': layer_names, 'points': geojson_dict}
+#
     if dumps:
         result = json.dumps(result)
 
@@ -85,7 +71,14 @@ def geojson_encoder_3(dataframe, fields=[], layer_names=[], lng='lng', lat='lat'
 
 
 def geojson_decoder_1(json_dict):
-    '''Takes a json dictionary and puts it into a pandas dataframe'''
+    '''
+    Deserialize an instance containing a GEOJSON
+
+    :param json_dict: Instance containg the GEOJSON data.
+                      Dictionary.
+
+    :return: Pandas DataFrame.
+    '''
     dframe = pd.DataFrame([pi['properties'] for pi in json_dict['features']])
     dframe['lng'] = [pi['geometry']['coordinates'][0] for pi in json_dict['features']]
     dframe['lat'] = [pi['geometry']['coordinates'][1] for pi in json_dict['features']]
